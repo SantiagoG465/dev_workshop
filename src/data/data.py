205d2df16192c4881df601fc3476ed9a592c9973
SantiagoG465 <santm099@gmail.com>
2025-03-19 21:01:22 -0500

    Merge ordenado

--- src/data/data.py
+++ src/data/data.py
@@ -33,25 +33,22 @@ class Data:
 
         return lista_sin_duplicados
     
-    def merge_ordenado(self, lista1, lista2):
-        """
-            i, j = 0, 0
-    resultado = []
-    
-    while i < len(lista1) and j < len(lista2):
-        if lista1[i] < lista2[j]:
-            resultado.append(lista1[i])
-            i += 1
-        else:
-            resultado.append(lista2[j])
-            j += 1
-    
-    resultado.extend(lista1[i:])
-    resultado.extend(lista2[j:])
-    
-    return resultado
-        """
-        pass
+    def merge_ordenado(self, lista1: list, lista2: list) -> list:
+        i, j = 0, 0
+        resultado = []
+        
+        while i < len(lista1) and j < len(lista2):
+            if lista1[i] < lista2[j]:
+                resultado.append(lista1[i])
+                i += 1
+            else:
+                resultado.append(lista2[j])
+                j += 1
+        
+        resultado.extend(lista1[i:])
+        resultado.extend(lista2[j:])
+        
+        return resultado
     
     def rotar_lista(self, lista, k):
         
